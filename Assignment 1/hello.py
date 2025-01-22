5dac08ffc10c6b8295fb0b2becdd7b641bf44f5e
Lekchen <rinchengyeltshen81@gmail.com>
2025-01-22 17:06:46 +0700

    Create  hello.py

---
+++ Assignment 1/hello.py
@@ -0,0 +1,7 @@
+# Assignment 1, Task 2
+# Name: Rinchen Lekchen Gyeltshen
+# Collaborators: kinchap Dubda
+# Time Spent: 1:30 hrs
+name = input("Please enter your name: ")
+x = len(name) * "!"
+print(f"Nice to meet you, {name}{x}")
