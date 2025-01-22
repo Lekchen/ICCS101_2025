7b53ec29cfa68a5bf0e18153d03392452e542fe2
Lekchen <rinchengyeltshen81@gmail.com>
2025-01-22 17:15:45 +0700

    Create  area.py

---
+++ Assignment 1/area.py
@@ -0,0 +1,13 @@
+# Assignment 1, Task 5
+# Name: Rinchen Lekchen Gyeltshen
+# Collaborators: John Nonexistent
+# Time Spent: 0:10 hrs
+
+p = float(input())
+q = float(input())
+pie = 3.14
+area_1 = (pie * ((q / 2) ** 2)) / 2
+area_2 = p * q
+area_3 = (pie * ((q / 2) ** 2)) / 2
+total_area = area_1 + area_2 + area_3
+print(F"The total area is {total_area}")
