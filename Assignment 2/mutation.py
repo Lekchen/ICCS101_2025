7ea9ccd0b16797fcee62cfb47607604a532f361d
Lekchen <rinchengyeltshen81@gmail.com>
2025-01-22 17:19:00 +0700

    Create mutation.py

---
+++ Assignment 2/mutation.py
@@ -0,0 +1,9 @@
+# Assignment 2, Task 1
+# Name: Rinchen Lekchen Gyeltshen
+# Collaborators:no one
+# Time Spent: 0:20 hrs
+s = input()
+t = input()
+s = s.lower()
+t = t.upper()
+s = s.replace("s", "m").replace("l", "m").replace("a", "m")
