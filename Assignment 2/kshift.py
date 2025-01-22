73baf5c22d9dd08a497c52b6388851364d444440
Lekchen <rinchengyeltshen81@gmail.com>
2025-01-22 17:20:11 +0700

    Create  kshift.py

---
+++ Assignment 2/kshift.py
@@ -0,0 +1,10 @@
+# Assignment 2, Task 3
+# Name: Rinchen Lekchen Gyeltshen
+# Collaborators:no one
+# Time Spent: 0:10 hrs
+st = str(input())
+k = int(input())
+first_part = st[-k:]
+remaining_part = st[:-k]
+result = first_part + remaining_part
+print(result)
