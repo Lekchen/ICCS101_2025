fb5de42781554d677a9f739d8cd250caba499dff
Lekchen <rinchengyeltshen81@gmail.com>
2025-02-11 20:27:10 +0700

    Update mutation.py

--- Assignment 2/mutation.py
+++ Assignment 2/mutation.py
@@ -7,3 +7,17 @@ t = input()
 s = s.lower()
 t = t.upper()
 s = s.replace("s", "m").replace("l", "m").replace("a", "m")
+t = t.replace("P", "T").replace("O", "T").replace("I", "T").replace("N", "T")
+first_s = s[0]
+first_t = t[0]
+remain_s = s[1:]
+remain_t = t[1:]
+s = first_t + remain_s
+t = first_s + remain_t
+one_third_s = int(len(s) / 3)
+one_third_t = int(len(t) / 3)
+remaining_s = s[:-one_third_s]
+middle_part = t[one_third_t: (one_third_t * 2)]
+s = remaining_s + middle_part
+z = s + t
+print(z)
