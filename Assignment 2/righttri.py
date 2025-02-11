690d022aa5f843e9e3e03601da228983b2802ef5
Lekchen <rinchengyeltshen81@gmail.com>
2025-02-11 20:45:20 +0700

    Update righttri.py

--- Assignment 2/righttri.py
+++ Assignment 2/righttri.py
@@ -5,10 +5,10 @@
 x: float = float(input())
 y: float = float(input())
 z: float = float(input())
-maxi_xy = int(((x + y) + abs(x - y)) // 2)
+maxi_xy = ((x + y) + abs(x - y)) // 2
 c = int(((maxi_xy + z) + abs(maxi_xy - z)) // 2)
 mini_xy = int(((x + y) - abs(x - y)) // 2)
-a = int(((mini_xy + z) - abs(mini_xy - z)) // 2)
+a = ((mini_xy + z) - abs(mini_xy - z)) // 2
 b = (x + y + z) - c - a
 result = abs(c**2 - (a**2 + b**2)) < 1e-7
 print(bool(result))
